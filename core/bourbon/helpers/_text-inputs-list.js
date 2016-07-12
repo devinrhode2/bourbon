20ebb061882c62f4c66cafe186f98dbd7e504bd5
Devin Rhode <devinrhode2@gmail.com>
2016-07-11 17:21:32 -0700

    ..porting helpers/_text-inputs-list

---
+++ core/bourbon/helpers/_text-inputs-list.js
@@ -0,0 +1,27 @@
+
+/// A list of all _text-based_ HTML inputs.
+///
+/// @type list
+///
+/// @access private
+
+//MUST NAME IMPORTED VAR "$_text-inputs-list"
+
+export default [
+  "[type='color']",
+  "[type='date']",
+  "[type='datetime']",
+  "[type='datetime-local']",
+  "[type='email']",
+  "[type='month']",
+  "[type='number']",
+  "[type='password']",
+  "[type='search']",
+  "[type='tel']",
+  "[type='text']",
+  "[type='time']",
+  "[type='url']",
+  "[type='week']",
+  "input:not([type])",
+  "textarea",
+];
